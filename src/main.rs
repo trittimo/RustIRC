66e28eaab19a48e67de0e24c1df1c2739ed8b72e
gilderjw <gilderjw@rose-hulman.edu>
2017-02-01 10:45:06 -0500

    pulled out command handling into another function when a NICK command is recieved, awknowledge it.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
+fn handle_command(command: &[u8], ref mut stream: &TcpStream) {
+  let command = String::from_utf8_lossy(command);
+  if command.starts_with("NICK") {
+    println!("recieved NICK command");
+    let _ = stream.write(":jeem NICK jeem\r\n".as_bytes());
+  }
+  // stream
+}
+
 fn handle_client(mut stream: TcpStream) {
   let mut buf;
   loop {
@@ -17,18 +26,20 @@ fn handle_client(mut stream: TcpStream) {
       },
     };
 
+    handle_command(&buf, &stream);
     println!("User at address {} said {}\n", stream.peer_addr().unwrap(), String::from_utf8_lossy(&buf) );
-    match stream.write(&buf) {
-      Err(_) => break,
-      Ok(_) => continue,
-    }
+
+    // match stream.write(&buf) {
+    //   Err(_) => break,
+    //   Ok(_) => continue,
+    // }
   }
 }
 // use std::io::Read;
 // use std::io::Write;
 
 fn main(){
-  let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+  let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
   for stream in listener.incoming() {
     match stream {
       Err(e) => { println!("failed: {}", e)}
