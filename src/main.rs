2935081d3a9355db6df0fc7bd34acad3805d60c9
Michael Trittin <michaeltrittin@gmail.com>
2017-02-05 01:31:17 -0500

    Switched from static junk to arc (which is preferred)

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,107 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
-#[macro_use] extern crate lazy_static;
-lazy_static! {
-  static ref GLOBAL_USERS: Mutex<Vec<User>> = Mutex::new(Vec::new());
-  static ref GLOBAL_CHANNELS: Mutex<Vec<Channel>> = Mutex::new(Vec::new());
-}
-
+#[allow(dead_code)]
 struct User {
   username: String,
+  realname: String,
   hostname: String,
   servername: String,
-  realname: String,
 }
 
 impl User {
-  fn new(name: &str, host: &str, servername: &str, realname: &str) -> 
-    User {User {username: String::from(name),
-                hostname: String::from(host), 
-                servername: String::from(servername),
-                realname: String::from(realname)
-               }
-         }
+  fn new(username: &str, realname: &str,
+         hostname: &str, servername: &str) -> User {
+    User {
+      username: String::from(username),
+      realname: String::from(realname),
+      hostname: String::from(hostname),
+      servername: String::from(servername)
+    }
+  }
 }
 
+#[allow(dead_code)]
 struct Channel {
-  channel_name: String,
+  name: String,
   topic: String,
   users: Vec<User>,
 }
 
 impl Channel {
-  fn new(c_name: &str, t_name: &str) -> 
-    Channel {Channel {channel_name: String::from(c_name),
-                      topic: String::from(t_name),
-                      users: Vec::new(),
-                    } 
-            }
+  fn new(name: &str, topic: &str) -> Channel {
+    Channel {
+      name: String::from(name),
+      topic: String::from(topic),
+      users: Vec::new()
+    }
+  }
 }
 
-fn handle_user(cmd: Vec<&str>, ref mut stream: &TcpStream) {
+#[allow(dead_code)]
+struct IRCState {
+  users: Vec<User>,
+  channels: Vec<Channel>,
+}
+
+impl IRCState {
+  fn new() -> IRCState {
+    IRCState {
+      users: Vec::new(),
+      channels: Vec::new(),
+    }
+  }
+  fn add_channel(&mut self, channel: Channel) {
+    self.channels.push(channel);
+  }
+}
+
+fn main() {
+  let mut state = IRCState::new();
+  let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
+  
+  // Add our dank channels
+  state.add_channel(Channel::new("Memes", "Where to find all the dankest memes"));
+  state.add_channel(Channel::new("Garbage", "where all the Otaku talk about anime [censored]"));
+
+  // Declare our shared state for threaded use
+  let shared_state = Arc::new(Mutex::new(state));
+
+  for stream in listener.incoming() {
+    match stream {
+      Err(e) => { println!("Error in stream: {}", e)}
+      Ok(stream) => {
+        let st = shared_state.clone();
+        thread::spawn(move || {
+          handle_client(stream, st);
+        });
+      }
+    }
+  }
+}
+
+fn handle_user(cmd: Vec<&str>, ref mut stream: &TcpStream, state: &Arc<Mutex<IRCState>>) {
   println!("recieved USER command");
-  let mut guard = GLOBAL_USERS.lock().unwrap();
-  guard.push(User::new(cmd[1], cmd[1], cmd[2], cmd[3]));
+  let ref mut users = state.lock().unwrap().users;
+  users.push(User::new(cmd[1], cmd[1], cmd[2], cmd[3]));
   let response = format!(":RustIRC Welcome to RustIRC!\r\n");
   let _ = stream.write(response.as_bytes());
 }
 
-fn handle_nick(cmd: Vec<&str>, ref mut stream: &TcpStream) {
+fn handle_nick(cmd: Vec<&str>, ref mut stream: &TcpStream, state: &Arc<Mutex<IRCState>>) {
   println!("recieved NICK command");
   let response = format!(":{0} NICK {0}\r\n", cmd[1]);
   let _ = stream.write(response.as_bytes());
 }
 
-fn handle_list(ref mut stream: &TcpStream) {
+fn handle_list(ref mut stream: &TcpStream, state: &Arc<Mutex<IRCState>>) {
   println!("recieved LIST command");
-  let guard = GLOBAL_CHANNELS.lock().unwrap(); //this line makes it so line 71 does not work
+  let ref mut channels = state.lock().unwrap().channels;
   let mut response : String = "".into();
-  for channel in guard.iter() {
-    response = response + "#" + channel.channel_name.as_str() + " ";
+  for channel in channels.iter() {
+    response = response + "#" + channel.name.as_str() + " ";
     let ref users = channel.users;
     for user in users {
       response = response + user.username.as_str() + ","
@@ -71,53 +112,35 @@ fn handle_list(ref mut stream: &TcpStream) {
   let _ = stream.write("alwl".as_bytes());
 }
 
-fn handle_command(cmd: &[u8], ref mut stream: &TcpStream) {
+fn handle_command(cmd: &[u8], ref mut stream: &TcpStream, state: &Arc<Mutex<IRCState>>) {
   let tmp = String::from_utf8_lossy(cmd);
   let command: Vec<&str> = tmp.split_whitespace().collect();
 
   match command[0] {
-      "NICK" => handle_nick(command, stream),
-      "USER" => handle_user(command, stream),
-      "LIST" => handle_list(stream),
+      "NICK" => handle_nick(command, stream, &state),
+      "USER" => handle_user(command, stream, &state),
+      "LIST" => handle_list(stream, &state),
       _ => println!("unknown command {}", command[0])
   }
   // stream
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, state: Arc<Mutex<IRCState>>) {
   let mut buf;
   loop {
     buf = [0; 512];
     let _ = match stream.read(&mut buf) {
-      Err(e) => panic!("Got error: {}", e),
+      Err(e) => panic!("Error handling client: {}", e),
       Ok(m) => {
         if m == 0 {
+          // TODO remove user from channel
           println!("User at address {} disconnected\n", stream.peer_addr().unwrap());
           break;
         }
-        m
       },
     };
 
-    handle_command(&buf, &stream);
+    handle_command(&buf, &stream, &state);
     println!("User at address {} said {}\n", stream.peer_addr().unwrap(), String::from_utf8_lossy(&buf) );
-  }
-}
-
-fn main(){
-  let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
-  let mut guard = GLOBAL_CHANNELS.lock().unwrap();
-  guard.push(Channel::new("memes", "where to find all the dankest memes"));
-  guard.push(Channel::new("anim00_garbage", "where all the Otaku talk about anime titties"));
-
-  for stream in listener.incoming() {
-    match stream {
-      Err(e) => { println!("failed: {}", e)}
-      Ok(stream) => {
-        thread::spawn(move || {
-          handle_client(stream)
-        });
-      }
-    }
   }
 }
