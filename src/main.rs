26827ecc30f807d519dc3774cb8781acee69c913
gilderjw <gilderjw@rose-hulman.edu>
2017-02-05 16:31:50 -0500

    fixed up list a bit

--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,7 @@ fn main() {
   let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
   
   // Add our dank channels
+
   state.add_channel(Channel::new("memes", "Where to find all the dankest memes"));
   state.add_channel(Channel::new("anim00_garbage", "where all the Otaku talk about anime [censored]"));
 
@@ -89,7 +90,7 @@ fn handle_user(cmd: Vec<&str>, ref mut stream: &TcpStream, state: &Arc<Mutex<IRC
   let mut response = format!("PING :3813401942\r\n");
   let _ = stream.write(response.as_bytes());
 
-  response = String::from(":localhost 001 jeem :Welcome to RustIRC!");
+  response = String::from(":localhost 001 jeem :Welcome to RustIRC!\r\n");
 
   let _ = stream.write(response.as_bytes());
 }
@@ -103,15 +104,14 @@ fn handle_nick(cmd: Vec<&str>, ref mut stream: &TcpStream, state: &Arc<Mutex<IRC
 fn handle_list(ref mut stream: &TcpStream, state: &Arc<Mutex<IRCState>>) {
   println!("recieved LIST command");
   let ref mut channels = state.lock().unwrap().channels;
-  let mut response : String = "".into();
+  let mut response : String = ":localhost 321 jeem Channel :Users  Name\r\n".into();
   for channel in channels.iter() {
-    response = response + "#" + channel.name.as_str() + " ";
-    let ref users = channel.users;
-    for user in users {
-      response = response + user.username.as_str() + ","
-    }
-    response = response + ":" + channel.topic.as_str() + "\r\n";
+    response = response + format!(":localhost 322 jeem #{0} {1} :{2}\r\n", 
+                                    channel.name.as_str(), 
+                                    channel.users.len(), 
+                                    channel.topic.as_str()).as_str();
   }
+  response = response + ":localhost 323 jeem :End of /LIST\r\n";
   let _ = stream.write(response.as_bytes());
 }
 
@@ -126,6 +126,11 @@ fn handle_ping(cmd: Vec<&str>, ref mut stream: &TcpStream) {
   let _ = stream.write(response.as_bytes()); 
 }
 
+fn handle_cap(ref mut stream: &TcpStream) {
+  let response = "CAP * LS :multi-prefix sasl=EXTERNAL";
+  let _ = stream.write(response.as_bytes());
+}
+
 fn handle_command(cmd: &[u8], ref mut stream: &TcpStream, state: &Arc<Mutex<IRCState>>) {
   let tmp = String::from_utf8_lossy(cmd);
   let command: Vec<&str> = tmp.split_whitespace().collect();
@@ -136,9 +141,10 @@ fn handle_command(cmd: &[u8], ref mut stream: &TcpStream, state: &Arc<Mutex<IRCS
       "LIST" => handle_list(stream, &state),
       "JOIN" => handle_join(command, stream, &state),
       "PING" => handle_ping(command, stream),
+      "CAP" => handle_cap(stream),
       _ => println!("unknown command {}", command[0])
   }
-  // stream
+  // stream 
 }
 
 fn handle_client(mut stream: TcpStream, state: Arc<Mutex<IRCState>>) {
